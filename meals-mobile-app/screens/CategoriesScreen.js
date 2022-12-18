c4ea445a9bb680be69cafa24f4657978b28b16f1
uditgandhi <coder.udit@gmail.com>
2022-12-18 19:25:50 -0400

    Basic screens setup.

---
+++ meals-mobile-app/screens/CategoriesScreen.js
@@ -0,0 +1,30 @@
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const CategoriesScreen = ({ navigation }) => {
+  //Should not be re-evaluated on component reload but to get hold of navigation property.
+
+  const renderCategoryItem = (itemData) => {
+    const onPressHandler = () => {
+      navigation.navigate("MealsOverview");
+    };
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={onPressHandler}
+      />
+    );
+  };
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
---
+++ meals-mobile-app/screens/MealsOverviewScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+const MealsOverviewScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Meals MealsOverviewScreen</Text>
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({ container: { flex: 1, padding: 16 } });
